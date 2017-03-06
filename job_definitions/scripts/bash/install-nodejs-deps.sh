2480f505e8dc16068e909d26ea5658f3e0c028a3
Stephen James <stephen.james@devopsguys.com>
2017-03-06 15:45:52 +0000

    Added source .bash rc to tests script.

--- job_definitions/scripts/bash/install-nodejs-deps.sh
+++ job_definitions/scripts/bash/install-nodejs-deps.sh
@@ -8,11 +8,10 @@ cd $CODE_PATH
 
 # and install the packages
 
-source "${HOME}/.bashrc"
-
 ### NODE JS can't read the SSL cert on the proxy
 export PHANTOMJS_CDNURL=http://cnpmjs.org/downloads
 #npm config set registry='http://registry.npmjs.org/'
+source "${HOME}/.bashrc"
 nvm install "${NODEJS_VERSION}"
 nvm use "${NODEJS_VERSION}"
 npm install
--- job_definitions/scripts/bash/run-nodejs-unit-tests.sh
+++ job_definitions/scripts/bash/run-nodejs-unit-tests.sh
@@ -10,6 +10,7 @@ CODE_PATH=$(echo $FULL_GLOB|cut -d '/' -f 1)
 # Now run the coverage command
 cd "${CODE_PATH}" || exit 1
 echo "Changing to $(pwd) and running npm test"
+source "${HOME}/.bashrc"
 nvm install "${NODEJS_VERSION}"
 nvm use "${NODEJS_VERSION}"
 npm install
