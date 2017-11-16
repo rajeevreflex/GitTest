c3f4e8e8776bdb8dcf0ebc416454c59234ee5902
Pratyush <p.mishra@ubiquedigital.com>
2017-11-16 22:06:12 +0530

    Creating new job for EAP-8326

---
+++ job_definitions/scripts/bash/cutrelease.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# use this from root of project and checkout to develop first to cut a release.
+# Before doing this update the CHANGELOG.md file.
+
+cutrelease() {
+   git checkout develop
+   git pull --rebase
+
+   #local last_tag=$(git describe --tags $(git rev-list --tags --max-count=1))
+   local last_tag=$(git for-each-ref --count=1 --format="%(refname:lstrip=-1)" --sort=-version:refname refs/tags)
+
+   echo "last tag: ${last_tag}"
+
+   echo -n "Enter release number [ENTER]: "
+   read new_release_version
+
+   echo "new  release branch: \"release/${new_release_version}\""
+
+   read -r -p "Are you sure? [y/N] " response
+   case $response in
+    [yY][eE][sS]|[yY])
+     echo "do release"
+     git checkout -b "release/${new_release_version}"
+     git push --set-upstream origin "release/${new_release_version}"
+     git checkout develop
+     git pull --rebase
+     ;;
+    *)
+     echo "aborted release branch creation"
+     ;;
+   esac
+}
+
+cutrelease
