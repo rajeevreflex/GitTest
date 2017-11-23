9fb21e355fe9fb4f85642a1a4dc88233f3b3c1d0
Pratyush <p.mishra@ubiquedigital.com>
2017-11-23 23:42:43 +0530

    Release branch created

--- job_definitions/scripts/bash/cutrelease.sh
+++ job_definitions/scripts/bash/cutrelease.sh
@@ -8,18 +8,35 @@ cutrelease() {
    git pull --rebase
    echo "do release"
    local last_release
-   last_release=$(git for-each-ref --count=1 --format="%(refname:lstrip=-2)" --sort=-version:refname refs/remotes/origin/release)
-   local last_release_full
-   last_release_full=$(git for-each-ref --count=1 --sort=-version:refname refs/remotes/origin/release)
+   last_release=$(git for-each-ref --count=1 --format="%(refname:short)" --sort=-committerdate refs/remotes/origin/release)
+   echo "======================================="
+   echo "last release: ${last_release}"
+   echo ${release_type}
 
+   release_type=${release_type}                     ## This value will be fetch from previous build
 
-   local last_tag
-   last_tag=$(git for-each-ref --count=1 --format="%(refname:lstrip=-1)" --sort=-version:refname refs/tags)
+   version=`echo ${last_release} | cut -b 23-`
+   a=( ${version//./ } )                   # replace points, split into array
+   #((a[2]++))                              # increment revision (or other part)
+   case $release_type in
+   	Major)
+   	((a[0]++))
+   	((a[1]=0))
+   	((a[2]=0))
+   		;;
+   	Minor)
+   	((a[1]++))
+   		;;
+   	Patch)
+   	((a[2]++))
+   		;;
+   	*)
+   		echo "Sorry, I don't understand"
+   		;;
+     esac
+     version="release${a[0]}.${a[1]}.${a[2]}"       # compose new version
+     echo ${version}
 
-   echo "======"
-   echo "     last release   : ${last_release}"
-   echo "last release full   : ${last_release_full}"
-   echo "         last tag   : ${last_tag}"
   #   git checkout -b "release/release_dev"  # This is hardcoded value for testing
   #   git push --set-upstream origin "release/release_dev" # This is hardcoded value for testing
   #   git checkout develop
