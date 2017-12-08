6c716d7522b4c966a33ba024370265fdffa805cb
Pratyush <p.mishra@ubiquedigital.com>
2017-12-08 17:42:03 +0530

    remove configuration

--- job_definitions/scripts/bash/test-application-code.sh
+++ job_definitions/scripts/bash/test-application-code.sh
@@ -2,18 +2,4 @@
 #
 ###
 echo "Place holder code for test-environment.sh"
-set appApiUrl=https://${ENVIRONMENT}api.dev.vf-leap.com
-set notificationApiUrl=https://${ENVIRONMENT}notifapi.dev.vf-leap.com
-set simAdmin=Carter
-set retailAgent=Albert
-set password=leapuser123
-FULL_GLOB=${WORKSPACE_CLONE_GLOB}
-CODE_PATH=$(echo $FULL_GLOB|cut -d '/' -f 1)
-JS_APP_FILE="${JS_APP_FILE}"
-
-cd "${CODE_PATH}" || exit 1
-
-echo "Running Node version: ${NODEJS_VERSION}"
-node --version
-npm run cleanSlateSanity -- --target=Stub
-#./run --test --environment "${ENVIRONMENT}" -y
+./run --test --environment "${ENVIRONMENT}" -y
